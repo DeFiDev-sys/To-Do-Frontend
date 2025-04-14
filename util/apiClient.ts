e533eb13b95c4f241d818b15a72f239cff8bb847
CodeGod36 <dynamox1x@gmil.com>
2025-04-14 21:48:29 +0100

    fix the api route in the apiClient instance

--- util/apiClient.ts
+++ util/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const apiClient = axios.create({
-    baseURL:process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+    baseURL:process.env.REACT_APP_API_URL || 'https://to-do-backend-6per.onrender.com',
     timeout:10000,
     withCredentials: true,
     headers:{

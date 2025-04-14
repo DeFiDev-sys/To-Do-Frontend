c803ee002c029bfef53b656abcb5baa532c7a67b
CodeGod36 <dynamox1x@gmil.com>
2025-04-14 21:48:29 +0100

    added supense for the reset-password page for the nextjs 14 version

--- app/(auths)/reset-password/page.tsx
+++ app/(auths)/reset-password/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 import ResetPassword from "@/Screens/PasswordScreen/ResetPassword";
 import { redirect, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
+import { BounceLoader } from "react-spinners";
 import { toast } from "sonner";
 
 const Page = () => {
+  return (
+    <Suspense
+      fallback={
+        <div className='flex justify-center items-center'>
+          <BounceLoader color='#0e25f1' />
+        </div>
+      }>
+      <ResetPasswordContent />
+    </Suspense>
+  );
+};
+
+const ResetPasswordContent = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token") as string;
 
@@ -14,8 +28,9 @@ const Page = () => {
       redirect("/SignIn");
     }
   }, [token]);
+
   return (
-    <div className='flex flex-col justify-center px-5  lg:px-10 2xl:px-20 min-h-dvh lg:shadow-2xl'>
+    <div className='flex flex-col justify-center px-5 lg:px-10 2xl:px-20 min-h-dvh lg:shadow-2xl'>
       <ResetPassword token={token} />
     </div>
   );
